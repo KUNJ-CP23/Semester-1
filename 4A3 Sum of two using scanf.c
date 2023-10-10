28066b8cc9baae510e6cff64a64f3a1809243b74
KUNJ-CP23 <140886416+KUNJ-CP23@users.noreply.github.com>
2023-10-10 11:48:03 +0530

    Add files via upload

---
+++ 4A3 Sum of two using scanf.c
@@ -0,0 +1,11 @@
+//Print sum of two with scanf
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	printf("Enter the value A\n");
+	scanf("%d",&a);
+	printf("Enter the value B\n");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Value of c = %d",c);
+}
