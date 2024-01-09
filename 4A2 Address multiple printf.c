a486e3b3fcfd24dbaa3edc91c25cbf7491b16b81
KUNJ-CP23 <140886416+KUNJ-CP23@users.noreply.github.com>
2024-01-09 17:18:19 +0530

    Delete 4A2 Address multiple printf.c

--- 4A2 Address multiple printf.c
+++
@@ -1,9 +0,0 @@
-//Print Address using multiple printf
-#include<stdio.h>
-void main(){
-	printf("Address ;\n");
-	printf("Street no. 3\n");
-	printf("Shraddha Puri,\n");
-	printf("80ft. Nehru Nagar Main Road,\n");
-	printf("Rajkot-360002.\n");
-}
